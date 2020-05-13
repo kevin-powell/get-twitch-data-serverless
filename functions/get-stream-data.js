28d8701cd6bb30f432e9b02d5dad68e9613f25f4
Jason Lengstorf <jason@lengstorf.com>
2020-05-12 20:22:07 -0700

    feat: demo site + better error handling

--- functions/get-stream-data.js
+++ functions/get-stream-data.js
@@ -4,14 +4,22 @@ const { getTwitchAccessToken } = require('@jlengstorf/get-twitch-oauth');
 exports.handler = async (event) => {
   const { user_login } = JSON.parse(event.body);
 
+  if (!process.env.TWITCH_CLIENT_ID || !process.env.TWITCH_CLIENT_SECRET) {
+    return {
+      statusCode: 401,
+      body: 'Must provide a Twitch app client ID and secret.',
+    };
+  }
+
   // get the server-to-server OAuth token from Twitch
   const { access_token } = await getTwitchAccessToken();
 
   // send an authenticated request to the Twitch API
   // see https://dev.twitch.tv/docs/api/reference#get-streams
   try {
+    const params = user_login ? `?user_login=${user_login}` : '';
     const response = await fetch(
-      `https://api.twitch.tv/helix/streams?user_login=${user_login}`,
+      `https://api.twitch.tv/helix/streams${params}`,
       {
         method: 'GET',
         headers: {
